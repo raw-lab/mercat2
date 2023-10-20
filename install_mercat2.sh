49823a87638ba4f6a5532db6b1c9d09ccb0f2437
Jose Figueroa <jlfiguer@uncc.edu>
2023-10-20 14:23:51 -0400

    Replaced conda with mamba, updated dependencies

--- install_mercat2.sh
+++ install_mercat2.sh
@@ -6,6 +6,6 @@ eval "$(conda shell.bash hook)"
 
 # create the mercat environment in conda
 ENV_NAME=mercat2
-conda create -n $ENV_NAME -c conda-forge -c bioconda grpcio python fastqc fastp prodigal metaomestats ray-core ray-dashboard ray-default configargparse pandas numpy humanize plotly psutil dominate scikit-learn scikit-bio scipy==1.8.1 python-kaleido -y
+mamba create -n $ENV_NAME -c bioconda -c conda-forge grpcio'=1.43' python">=3.9" fastqc fastp prodigal metaomestats ray-core ray-dashboard ray-default ray-tune configargparse pandas numpy humanize plotly psutil dominate scikit-learn scikit-bio scipy python-kaleido -y
 conda activate $ENV_NAME
 pip install .
