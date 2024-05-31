748efb92abdb3ce0289302c5e7f71df79a014f4e
Jose Figueroa <jlfiguer@uncc.edu>
2024-05-30 21:18:18 -0400

    Added matplotlib dependency

--- install_mercat2.sh
+++ install_mercat2.sh
@@ -6,6 +6,6 @@ eval "$(conda shell.bash hook)"
 
 # create the mercat environment in conda
 ENV_NAME=mercat2-1.4.1
-mamba create -n $ENV_NAME -c bioconda -c conda-forge grpcio'=1.43' python">=3.9" setuptools"<70.0.0" fastqc fastp prodigal metaomestats ray-core ray-dashboard ray-default ray-tune configargparse pandas numpy humanize plotly psutil dominate scikit-learn scikit-bio scipy python-kaleido -y
+mamba create -n $ENV_NAME -c bioconda -c conda-forge grpcio'=1.43' python">=3.9" setuptools"<70.0.0" fastqc fastp prodigal metaomestats ray-core ray-dashboard ray-default ray-tune configargparse pandas numpy humanize plotly psutil dominate scikit-learn scikit-bio scipy matplotlib python-kaleido -y
 conda activate $ENV_NAME
 pip install .
