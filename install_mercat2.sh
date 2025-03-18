263d59062b7abbf17638df4131d11fe864015e91
Jose Figueroa <jlfiguer@uncc.edu>
2025-03-18 17:36:26 -0400

    Updated dependencies

--- install_mercat2.sh
+++ install_mercat2.sh
@@ -5,7 +5,11 @@ set -e
 eval "$(conda shell.bash hook)"
 
 # create the mercat environment in conda
-ENV_NAME=mercat2
-mamba create -n $ENV_NAME -c bioconda -c conda-forge grpcio'=1.43' python">=3.9" fastqc fastp prodigal metaomestats ray-core ray-dashboard ray-default ray-tune configargparse pandas numpy humanize plotly psutil dominate scikit-learn scikit-bio scipy python-kaleido -y
+ENV_NAME=MerCat2
+
+mamba create -n $ENV_NAME -y -c conda-forge -c bioconda python">=3.9" \
+	fastqc fastp prodigal metaomestats \
+	hydrampp configargparse humanize plotly psutil dominate scikit-learn scikit-bio scipy python-kaleido matplotlib
+
 conda activate $ENV_NAME
 pip install .
