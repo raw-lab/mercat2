d13a59373d428107cbdd9d3ef535ef1a9cc0e8d5
Jose Figueroa <jlfiguer@uncc.edu>
2026-06-09 13:12:26 -0400

    Updated dependencies

--- install_mercat2.sh
+++ install_mercat2.sh
@@ -8,8 +8,8 @@ eval "$(conda shell.bash hook)"
 ENV_NAME=MerCat2
 
 mamba create -n $ENV_NAME -y -c conda-forge -c bioconda python">=3.9" \
-	fastqc fastp prodigal metaomestats \
-	hydrampp configargparse humanize plotly psutil dominate scikit-learn scikit-bio scipy python-kaleido matplotlib
+	fastqc fastp metaomestats \
+	hydrampp configargparse humanize plotly psutil dominate scikit-learn scikit-bio scipy python-kaleido matplotlib pyrodigal
 
 conda activate $ENV_NAME
 pip install .
